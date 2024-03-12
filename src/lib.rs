41f33828d766e11dd74c68c92acd822ad9b8a02a
Simon Goller <neosam+github@posteo.de>
2024-03-12 10:22:22 +0100

    Add documentation to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! # Some bevy tools
+//!
+//! This repo contains extensions for the great [Bevy Engine](https://bevyengine.org/). My goal is to have a crate which
+//! provides ECS stuff I regularly use in projects to save me time in future Bevy Game Jams.  This crate tries to
+//! make the usage as simple as possible so the developer can focus on the main content of the game.
+//!
+//! Currently supported features are:
+//!
+//! * Automatic despawn after a period of time.
+//! * Automatic despawn of components on a state change.
+//! * Range component which keeps its value between a min and a max value and writes events
+//!   if min or max was reached.  For example it can be used for health to detect death.
+//! * Simplified processing of events on collisions in rapier.
+//! * Mapping of user inputs to custom events. (currently only keyboard events are supported for now)
+//! * Loading of assets on a loading state and storing them automatically in a resource using reflect.
+//! * Split screen support.
+//! * SBS support. It is basically a split screen which allows a sterioscopic view by using special
+//!   hardware like XReal or Virture glasses.
+//!
+//! Additionally, I try to document each module with at least one example. This should ensure that
+//! there are no accidential breaking changes.
+
 #[cfg(feature = "bevy_rapier2d")]
 pub mod collision_detection;
 #[cfg(feature = "bevy_rapier3d")]
