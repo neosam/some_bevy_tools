9e4a0fef59f1d7b791b7847faa2401d3a372bee7
Simon Goller <neosam+github@posteo.de>
2024-03-29 17:04:15 +0100

    Use audio_loop in loading example

--- examples/loading.rs
+++ examples/loading.rs
@@ -1,5 +1,8 @@
 use bevy::prelude::*;
-use some_bevy_tools::loading as easy_loading;
+use some_bevy_tools::{
+    audio_loop::{AudioLoopPlugin, LoopableAudioSource},
+    loading as easy_loading,
+};
 
 #[derive(States, PartialEq, Eq, Debug, Default, Hash, Clone, Copy)]
 pub enum GameState {
@@ -19,9 +22,21 @@ impl easy_loading::EasyAssetLoader for TextureAssets {
     }
 }
 
+#[derive(Resource, Default, Reflect, Clone)]
+pub struct AudioAssets {
+    pub music: Handle<LoopableAudioSource>,
+}
+impl easy_loading::EasyAssetLoader for AudioAssets {
+    type AssetType = LoopableAudioSource;
+    fn asset_mapper() -> &'static [(&'static str, &'static str)] {
+        &[("music", "ehh-ehh.ogg")]
+    }
+}
+
 pub fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(AudioLoopPlugin)
         .add_plugins(easy_loading::LoadingPlugin(
             GameState::Loading,
             GameState::InGame,
@@ -30,16 +45,24 @@ pub fn main() {
             TextureAssets::default(),
             GameState::Loading,
         ))
+        .add_plugins(easy_loading::LoadPluginAssets(
+            AudioAssets::default(),
+            GameState::Loading,
+        ))
         .init_state::<GameState>()
         .add_systems(OnEnter(GameState::InGame), init_ingame)
         .run();
 }
 
-pub fn init_ingame(mut commands: Commands, assets: Res<TextureAssets>) {
+pub fn init_ingame(mut commands: Commands, assets: Res<TextureAssets>, audio: Res<AudioAssets>) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
         texture: assets.ducky.clone(),
         transform: Transform::from_xyz(300.0, 0.0, 0.0),
+        ..Default::default()
+    });
+    commands.spawn(AudioSourceBundle {
+        source: audio.music.clone(),
         ..Default::default()
     });
 }
